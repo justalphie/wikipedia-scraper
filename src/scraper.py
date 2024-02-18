d8887d9233ed92d9e6e39a1108f944f3adcc4607
justalphie <54706142+justalphie@users.noreply.github.com>
2024-02-18 14:49:20 +0100

    Add function get_with_cookies

--- src/scraper.py
+++ src/scraper.py
@@ -11,19 +11,36 @@ class WikipediaScraper:
         self.leaders_endpoint = base_url+"/leaders"
         self.cookies_endpoint = base_url+"/cookie"
         self.leaders_data = {}
-        self.cookie = requests.get(self.cookies_endpoint).cookies
+        self.session = Session()
+        self.cookies = self.session.get(self.cookies_endpoint).cookies
 
     def __str__(self):
         return f"WikipediaScraper requests the {self.base_url} API to get data about the leaders of the countries in the list."
 
     def refresh_cookie(self) -> object:
-        return requests.get(self.cookies_endpoint).cookies
+        self.cookies = self.session.get(self.cookies_endpoint).cookies
+
+    def get_with_cookies(self, url, params={}):
+        try:
+            result = self.session.get(url, params, cookies=self.cookies)
+            if result.ok: 
+                return result
+            else:
+                # refresh the cookie
+                self.refresh_cookie()
+                # try again
+                return self.get_with_cookies(url, params)                
+        except:
+            # refresh the cookie
+            self.refresh_cookie()
+            # try again
+            return self.get_with_cookies(url, params)
 
     def get_countries(self) -> list:
-        return requests.get(self.country_endpoint, cookies=self.cookie).json()
+        return self.get_with_cookies(self.country_endpoint).json()
 
-    def get_first_paragraph(self, wikipedia_url: str, session = Session()) -> str:
-        r = session.get(wikipedia_url)
+    def get_first_paragraph(self, wikipedia_url: str) -> str:
+        r = self.session.get(wikipedia_url)
         soup = BeautifulSoup(r.content, "html.parser")
         paragraphs =  soup.select("#mw-content-text > div.mw-parser-output > p")
         paragraphs = [p for p in paragraphs if len(p.text.strip())>0]
@@ -34,12 +51,12 @@ class WikipediaScraper:
         return first_paragraph
 
     def get_leaders(self, country: str) -> None:
-        session = Session()
         params = {"country":f"{country}"}
-        self.leaders_data[country] = session.get(self.leaders_endpoint, cookies=self.refresh_cookie(), params=params).json()
+        self.leaders_data[country] = self.get_with_cookies(self.leaders_endpoint, params=params).json()
         for i in range(len(self.leaders_data[country])):
-            self.leaders_data[country][i]["first_paragraph"] = self.get_first_paragraph(self.leaders_data[country][i]["wikipedia_url"], session)
-        
+            self.leaders_data[country][i]["first_paragraph"] = self.get_first_paragraph(self.leaders_data[country][i]["wikipedia_url"])
+
+   
     def to_json_file(self, filepath: str) -> None:
         with open (filepath, "w", encoding="utf-8") as f:
             json.dump(self.leaders_data, f, indent=4, ensure_ascii = False)
